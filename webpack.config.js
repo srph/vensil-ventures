ad8266b3d53d2eb326c18e7ead6c6c572d2c2fb4
srph <seraphipod@gmail.com>
2015-03-20 16:55:50 +0800

    Added webpack config.

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: './src/App.jsx',
+  output: {
+    filename: 'script.js', // Filename of the output
+    path: './dist' // Directory of the output
+  },
+  module: {
+    // Transformers
+    loaders: [
+      // Transform to es6 with babel
+      // http://webpack.github.io/docs/loaders.html
+      { test: /\.(js|jsx|es6)$/, exclude: 'node_modules/', loader: 'babel-loader?modules=common&experimental=true' }
+    ]
+  },
+  resolve: {
+    // So we can require files without specfiying the file extension.
+    // e.g., require('./yolo.es6') => require('./yolo');
+    extensions: ['', '.js', '.json', '.jsx', '.es6']
+  }
+}
