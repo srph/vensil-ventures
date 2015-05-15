e3cd530dbc90dd87d193145ecc5b93f858d6e501
srph <seraphipod@gmail.com>
2015-05-16 06:46:51 +0800

    Buttons!

--- src/handlers/Home/SeeServicesButton.jsx
+++ src/handlers/Home/SeeServicesButton.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class SeeServicesButton extends React.Component {
   render() {
     return (
-      <a className="home__seeServicesBtn" href="#" onClick={this._handleClick}>
+      <a className="home__service-btn" href="#" onClick={this._handleClick}>
         See Services
       </a>
     );
