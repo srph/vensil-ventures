8f1cd138f547699cbef268a8744161d361277b12
srph <seraphipod@gmail.com>
2015-05-16 20:51:26 +0800

    Navigation and home.

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import bean from 'bean';
+import classnames from 'classnames';
 
 export default class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { fixed: false };
+  }
+
+  componentDidMount() {
+    bean.on(window, 'scroll', this._handleScroll.bind(this));
+  }
+
+  componentWillUnmount() {
+   bean.on(window, 'scroll', this._handleScroll.bind(this));
+  }
+
   render() {
+    let classname = classnames('nav', { 'nav--sticky': this.state.fixed });
+
     return (
-      <nav className="nav" role="navigation">
+      <nav className={classname} role="navigation">
         <ul className="nav__navbar">
           <li><a href="#">Home</a></li>
           <li><a href="#">About</a></li>
@@ -13,5 +30,10 @@ export default class Navigation extends React.Component {
         </ul>
       </nav>
     );
+  }
+
+  _handleScroll() {
+    // Set navbar to fixed if it has exceeded .home
+    this.setState({ fixed: window.scrollY >= window.innerHeight });
   }
 }
--- src/handlers/Home/index.js
+++ src/handlers/Home/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Navigation from '../../components/Navigation';
 import SeeServicesButton from './SeeServicesButton';
 
 export default class Home extends React.Component {
@@ -7,13 +8,15 @@ export default class Home extends React.Component {
       <div className="home">
         <div className="home__outer">
           <div className="home__inner">
-            <div className="g-container">
+            <div className="home__container">
               <img src="" className="home__logo" />
               <h5> Vensils Ventures is a photobooth services on Metro Manila </h5>
               <SeeServicesButton />
             </div>
           </div>
         </div>
+
+        <Navigation />
       </div>
     );
   }
