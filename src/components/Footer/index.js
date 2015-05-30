25c9c671b5bef78da1ac75610f8c2ae5d8189221
srph <seraphipod@gmail.com>
2015-05-31 07:10:54 +0800

    Upd8.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,13 +9,15 @@ export default class Footer extends React.Component {
             <div className="footer__copyright">Vensils Ventures 2015+.</div>
 
             <div className="footer__social-media">
-              <a className="footer__social-media__icon footer__social-media__icon--fb" href="">
-                <i className="fa fa-facebook" />
-              </a>
+              <div className="footer__social-media__container">
+                <a className="footer__social-media__icon footer__social-media__icon--fb" href="">
+                  <i className="fa fa-facebook" />
+                </a>
 
-              <a className="footer__social-media__icon footer__social-media__icon--tw" href="">
-                <i className="fa fa-twitter" />
-              </a>
+                <a className="footer__social-media__icon footer__social-media__icon--tw" href="">
+                  <i className="fa fa-twitter" />
+                </a>
+              </div>
             </div>
           </div>
         </div>
--- src/handlers/About/index.js
+++ src/handlers/About/index.js
@@ -4,6 +4,9 @@ export default class About extends React.Component {
   render() {
     return (
       <div className="about">
+        <div className="about__container">
+          Hello
+        </div>
       </div>
     );
   }
