52413f52e321c9f556d9109f0b0c70005f774116
srph <seraphipod@gmail.com>
2015-05-31 21:22:00 +0800

    Converted dbl-quotes to sgl-quotes to fix linting errors.

--- src/handlers/About/data.js
+++ src/handlers/About/data.js
@@ -1,12 +1,12 @@
 export default [
-  { "title": "Raine @ 7", "date": "May 18, 2015", "thumbnail": "" },
-  { "title": "Santino's 7th Birthday", "date": "May 10, 2015", "thumbnail": "" },
-  { "title": "Nino & Mharj", "date": "May 18, 2015", "thumbnail": "" },
-  { "title": "Jhun-Eden Wedding", "date": "April 28, 2015", "thumbnail": "" },
-  { "title": "OLSHS Gratitude Night & Grad Ball", "date": "March 14, 2015", "thumbnail": "" },
-  { "title": "NJS High School Ball", "date": "February 2, 2015", "thumbnail": "" },
-  { "title": "Mike & Mie", "date": "January 01, 2015", "thumbnail": "" },
-  { "title": "Fritz & Whenna", "date": "December 26, 2014", "thumbnail": "" },
-  { "title": "Mharee Irish", "date": "December 20, 2014", "thumbnail": "" },
-  { "title": "Shirlyn Joy 18th Birthday", "date": "October 22, 2014", "thumbnail": "" }
+  { 'title': 'Raine @ 7', 'date': 'May 18, 2015', 'thumbnail': '' },
+  { 'title': 'Santino\'s 7th Birthday', 'date': 'May 10, 2015', 'thumbnail': '' },
+  { 'title': 'Nino & Mharj', 'date': 'May 18, 2015', 'thumbnail': '' },
+  { 'title': 'Jhun-Eden Wedding', 'date': 'April 28, 2015', 'thumbnail': '' },
+  { 'title': 'OLSHS Gratitude Night & Grad Ball', 'date': 'March 14, 2015', 'thumbnail': '' },
+  { 'title': 'NJS High School Ball', 'date': 'February 2, 2015', 'thumbnail': '' },
+  { 'title': 'Mike & Mie', 'date': 'January 01, 2015', 'thumbnail': '' },
+  { 'title': 'Fritz & Whenna', 'date': 'December 26, 2014', 'thumbnail': '' },
+  { 'title': 'Mharee Irish', 'date': 'December 20, 2014', 'thumbnail': '' },
+  { 'title': 'Shirlyn Joy 18th Birthday', 'date': 'October 22, 2014', 'thumbnail': '' }
 ];
