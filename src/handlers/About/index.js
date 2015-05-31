b46d38238c0ba273195cb423c29e30dceac1dde5
srph <seraphipod@gmail.com>
2015-05-31 18:25:53 +0800

    Updated About handler; added scrollable container and card stylign. Init data for the About cards.

--- src/handlers/About/index.js
+++ src/handlers/About/index.js
@@ -5,7 +5,14 @@ export default class About extends React.Component {
     return (
       <div className="about">
         <div className="about__container">
-          Hello
+          <div className="about__container__inner">
+            <div className="about__card">
+              <header className="about__card__info">
+                <div className="about__card__info__heading">Kier's 18th Birthday</div>
+                <div className="about__card__info__date">May 18, 2015</div>
+              </header>
+            </div>
+          </div>
         </div>
       </div>
     );
