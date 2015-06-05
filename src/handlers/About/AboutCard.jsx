c2c21dcf41b5d21ebb7b990674e00f17b390713d
srph <seraphipod@gmail.com>
2015-06-05 22:07:37 +0800

    Added initial fb album URL.
    Added note and require rule for the propType.

--- src/handlers/About/AboutCard.jsx
+++ src/handlers/About/AboutCard.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 
 export default class AboutCard extends React.Component {
-  static propTypes = { data: React.PropTypes.object };
+  static propTypes = {
+    /**
+     * Thumbnail data
+     */
+    data: React.PropTypes.object.isRequired
+  };
 
   constructor(props) {
     super(props);
   }
 
   render() {
-    let { title, date, thumbnail } = this.props.data;
+    let { title, date, thumbnail, set } = this.props.data;
 
     return (
       <li className="about__card">
@@ -16,7 +21,7 @@ export default class AboutCard extends React.Component {
         <header className="about__card__info">
           <div className="about__card__info__heading">{title}</div>
           <div className="about__card__info__date">{date}</div>
-          <a href="#" className="about__card__info__more-btn">More Photos</a>
+          <a href={`//facebook.com/media/set/?set=${set}`} className="about__card__info__more-btn">More Photos</a>
         </header>
       </li>
     );
