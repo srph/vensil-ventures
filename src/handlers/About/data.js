01479f3de491af1d0b81ed9a6d3a54c55677c72e
Kier Borromeo <seraphipod@gmail.com>
2015-06-05 14:20:11 +0800

    Added thumbnail data

--- src/handlers/About/data.js
+++ src/handlers/About/data.js
@@ -1,18 +1,18 @@
 export default [
   [
-    { 'title': 'Raine @ 7', 'date': 'May 18, 2015', 'thumbnail': '' },
-    { 'title': 'Santino\'s 7th Birthday', 'date': 'May 10, 2015', 'thumbnail': '' },
-    { 'title': 'Nino & Mharj', 'date': 'May 18, 2015', 'thumbnail': '' }
+    { 'title': 'Raine @ 7', 'date': 'May 18, 2015', 'thumbnail': 'raine-at-7' },
+    { 'title': 'Santino\'s 7th Birthday', 'date': 'May 10, 2015', 'thumbnail': 'santino-turns-7' },
+    { 'title': 'Nino & Mharj', 'date': 'May 18, 2015', 'thumbnail': 'nino-mharj' }
   ],
   [
-    { 'title': 'Jhun-Eden Wedding', 'date': 'April 28, 2015', 'thumbnail': '' },
-    { 'title': 'OLSHS Gratitude Night & Grad Ball', 'date': 'March 14, 2015', 'thumbnail': '' },
-    { 'title': 'NJS High School Ball', 'date': 'February 2, 2015', 'thumbnail': '' },
-    { 'title': 'Mike & Mie', 'date': 'January 01, 2015', 'thumbnail': '' }
+    { 'title': 'Jhun-Eden Wedding', 'date': 'April 28, 2015', 'thumbnail': 'jhun-eden-wedding' },
+    { 'title': 'OLSHS Gratitude Night & Grad Ball', 'date': 'March 14, 2015', 'thumbnail': 'olshs-gratitude-night-grad-ball' },
+    { 'title': 'NJS High School Ball', 'date': 'February 2, 2015', 'thumbnail': 'njs-high-school-ball' },
+    { 'title': 'Mike & Mie', 'date': 'January 01, 2015', 'thumbnail': 'mike-mie' }
   ],
   [
-    { 'title': 'Fritz & Whenna', 'date': 'December 26, 2014', 'thumbnail': '' },
-    { 'title': 'Mharee Irish', 'date': 'December 20, 2014', 'thumbnail': '' },
-    { 'title': 'Shirlyn Joy 18th Birthday', 'date': 'October 22, 2014', 'thumbnail': '' }
+    { 'title': 'Fritz & Whenna', 'date': 'December 26, 2014', 'thumbnail': 'fritz-wheena' },
+    { 'title': 'Mharee Irish', 'date': 'December 20, 2014', 'thumbnail': 'mharee-irish' },
+    { 'title': 'Shirlyn Joy 18th Birthday', 'date': 'October 22, 2014', 'thumbnail': 'shirlyn-joy-18th-birthday' }
   ]
 ];
