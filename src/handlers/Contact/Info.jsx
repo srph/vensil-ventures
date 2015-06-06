e1c58aed476004a52dd455b7a5f9e05118b48469
srph <seraphipod@gmail.com>
2015-06-06 22:42:11 +0800

    .

--- src/handlers/Contact/Info.jsx
+++ src/handlers/Contact/Info.jsx
@@ -5,20 +5,26 @@ export default class Info extends React.Component {
     return (
       <div className="contact__info">
         <div className="contact__info__heading">Contact Us</div>
+        <section className="contact__info__content">
+          <div className="contact__info__content__heading">Office</div>
+          <span className="contact__info__highlight">28 Cordillera Street, Galas, 1113, Quezon City, Metro Manila, Philippines</span>
+        </section>
 
-        <div className="contact__info__content">
-          <p>
-            Our office is located at <span className="contact__info__highlight">28 Cordillera Street, Galas, 1113, Quezon City</span>.
-            Feel free to drop by our email to say hello, <span className="contact__info__highlight">hello@vensils.com</span>.
-          </p>
+        <section className="contact__info__content">
+          <div className="contact__info__content__heading">Email</div>
+          <span className="contact__info__highlight">hello@vensils.com</span>
+        </section>
 
-          <p>
-            If you prefer, you can also contact us through phone with any of the following numbers:{' '}
-            <span className="contact__info__highlight">09164417383</span>,{' '}
-            <span className="contact__info__highlight">09164417383</span>,{' '}
-            <span className="contact__info__highlight">09164417383</span>.
-          </p>
-        </div>
+        <section className="contact__info__content">
+          <div className="contact__info__content__heading">Mobile</div>
+          <span className="contact__info__highlight">09164417383</span>,{' '}
+          <span className="contact__info__highlight">09164417383</span>,{' '}
+          <span className="contact__info__highlight">09164417383</span>.
+        </section>
+
+        <a href="#" className="contact__info__email-btn">
+          Send us an email!
+        </a>
       </div>
     );
   }
