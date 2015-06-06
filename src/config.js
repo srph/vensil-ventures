cde83e1e099ae86dcc7527258b35d9d023dbd3a2
srph <seraphipod@gmail.com>
2015-06-07 00:33:39 +0800

    Updated email-related links with an email link. Improved contact highlight styling for links.
    Added a config for all the basic data (phone, address, etc).

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export default {
+  address: '28 Cordillera Street, Galas, 1113, Quezon City, Metro Manila, Philippines',
+  email: 'hello@vensils.com',
+  phone: '732-0201'
+};
--- src/handlers/Contact/Info.jsx
+++ src/handlers/Contact/Info.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
+import config from '../../config';
 
 export default class Info extends React.Component {
   render() {
+
     return (
       <div className="contact__info">
         <div className="contact__info__heading">Contact Us</div>
         <section className="contact__info__content">
           <div className="contact__info__content__heading">Office</div>
-          <span className="contact__info__highlight">28 Cordillera Street, Galas, 1113, Quezon City, Metro Manila, Philippines</span>
+          <span className="contact__info__highlight">{config.address}</span>
         </section>
 
         <section className="contact__info__content">
           <div className="contact__info__content__heading">Email</div>
-          <span className="contact__info__highlight">hello@vensils.com</span>
+          <a href={`emailto:${config.email}`} className="contact__info__highlight">{config.email}</a>
 
           <div className="contact__info__email-btn-container">
-            <a href="#" className="contact__info__email-btn"> Send us an email! </a>
+            <a href={`emailto:${config.email}`} className="contact__info__email-btn"> Send us an email! </a>
           </div>
         </section>
 
         <section className="contact__info__content">
-          <div className="contact__info__content__heading">Mobile</div>
-          <span className="contact__info__highlight">09164417383</span>,{' '}
-          <span className="contact__info__highlight">09164417383</span>,{' '}
-          <span className="contact__info__highlight">09164417383</span>.
+          <div className="contact__info__content__heading">Phone</div>
+          <span className="contact__info__highlight">{config.phone}</span>
         </section>
       </div>
     );
