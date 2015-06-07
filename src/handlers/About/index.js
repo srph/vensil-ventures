239d912df95f71bab644a5e44911eb2c952f27cd
srph <seraphipod@gmail.com>
2015-06-07 12:55:12 +0800

    Added card content and distinguishable bg.

--- src/handlers/About/index.js
+++ src/handlers/About/index.js
@@ -6,18 +6,46 @@ export default class About extends React.Component {
       <div className="about">
         <div className="about__container">
           <div className="about__container__inner">
-            <ul className="about__card-container about__card-container--full">
-              <li className="about__card"></li>
+            <ul className="about__card-container about__card-container">
+              <li className="about__card about__card--main">
+                <div className="about__card__heading"> About Us </div>
+                <div className="about__card__info">
+                  Vensil's Ventures is a photo booth service based on Manila. Although, we also offer other services; &nbsp;
+                  Interested? Then read on!
+                </div>
+              </li>
+
+              <li className="about__card about__card--lighter">
+                <div className="about__card__heading"> Photography </div>
+                <div className="about__card__info">
+                  Besides photo booths, we also offer high-grade photographs for school pictorials, toga pictures, and etc.
+                </div>
+              </li>
             </ul>
 
             <ul className="about__card-container about__card-container--half">
-              <li className="about__card"></li>
-              <li className="about__card"></li>
+              <li className="about__card">
+                <div className="about__card__heading"> Editing </div>
+                <div className="about__card__info">
+                  We also provide digital editing to improve any raw photos.
+                </div>
+              </li>
+
+              <li className="about__card about__card--gray">
+                <div className="about__card__heading"> Video </div>
+                <div className="about__card__info">
+                  Tangina di ko na alam sasabihin ko.
+                </div>
+              </li>
             </ul>
 
-            <ul className="about__card-container about__card-container">
-              <li className="about__card"></li>
-              <li className="about__card"></li>
+            <ul className="about__card-container about__card-container--full">
+              <li className="about__card about__card--sub">
+                <div className="about__card__heading"> Photo Booth </div>
+                <div className="about__card__info">
+                  This service is our forte.
+                </div>
+              </li>
             </ul>
           </div>
         </div>
