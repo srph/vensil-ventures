0ef8680b7a408ca370f23ef29b6e84bda0cdb474
srph <seraphipod@gmail.com>
2015-06-09 01:34:13 +0800

    Reword.

--- src/handlers/Home/ServicesButton.jsx
+++ src/handlers/Home/ServicesButton.jsx
@@ -4,7 +4,7 @@ export default class ServicesButton extends React.Component {
   render() {
     return (
       <a className="home__service-btn" href="#" onClick={this._handleClick}>
-        Check Our Services
+        Check Our Work
         <span className="home__service-btn__caret">
           <i className="fa fa-caret-right"></i>
         </span>
