f8346e872ca8d22c197850aded285896124f3d88
srph <seraphipod@gmail.com>
2015-06-08 23:12:52 +0800

    Added heading for work and about for mobile screens.

--- src/handlers/Work/index.js
+++ src/handlers/Work/index.js
@@ -6,6 +6,10 @@ export default class Work extends React.Component {
   render() {
     return (
       <div className="work">
+        <div className="work__heading">
+          <div className="work__heading__title">Work</div>
+        </div>
+
         <div className="work__container">
           <div className="work__container__inner">
             {set.map((list, i) => (
