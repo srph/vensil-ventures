e7167e167576c795b17a390f5bd722a1f362e7ee
srph <seraphipod@gmail.com>
2015-06-09 16:12:21 +0800

    Removed parallax opacity temporarily.
    The website gets so slow.

--- src/handlers/Home/index.js
+++ src/handlers/Home/index.js
@@ -3,7 +3,7 @@ import ServicesButton from './ServicesButton';
 import ContactButton from './ContactButton';
 
 import Navigation from '../../components/Navigation';
-import ParallaxOpacity from '../../components/ParallaxOpacity';
+// import ParallaxOpacity from '../../components/ParallaxOpacity';
 
 export default class Home extends React.Component {
   render() {
@@ -11,13 +11,11 @@ export default class Home extends React.Component {
       <div className="home" id="home">
         <div className="home__outer">
           <div className="home__inner">
-            <ParallaxOpacity>
-              <div className="home__container">
-                <div className="home__heading"> Let's make awesome memories! </div>
-                <ContactButton />
-                <ServicesButton />
-              </div>
-            </ParallaxOpacity>
+            <div className="home__container">
+              <div className="home__heading"> Let's make awesome memories! </div>
+              <ContactButton />
+              <ServicesButton />
+            </div>
           </div>
         </div>
 
